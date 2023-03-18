bd4b85dc8e1def36a168d5a0787d1153a61f2c7a
Hilde Jacobi <p3xs9rskkdc6-student@anonymous-codio.com>
2023-03-18 09:51:09 +0000

    commit 1: change hour into milliseconds using method getTime () - Thursday 16 march 2023

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,12 +10,17 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+        long duration = (outHour - inHour)/ 3600000 ;
+        // IN 1 hour = 3600 *1000 milisecond = 3 600 000 Milliseconde
+        //1 Heure [h] = 3 600 000 Milliseconde [ms] 
+        // Duration in hour = Duration in milisecond   divide by  3 600 000 Milliseconde [ms] 
+        // DURATION inHOUR =  long inHour / 3 600 000
+        // DURATION out HOUR =  long outHour / 3 600 000
+        // => long duration IN HOUR = (outHour - inHour) / 3 600 000
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
