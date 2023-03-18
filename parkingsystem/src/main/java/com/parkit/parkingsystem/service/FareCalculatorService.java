1e79d97fdf0986aaaf4981db58a7839b33661f72
Hilde Jacobi <p3xs9rskkdc6-student@anonymous-codio.com>
2023-03-18 21:41:28 +0000

    test 1 commit

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,7 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+
 public class FareCalculatorService {
 // TEST COMMIT 
     public void calculateFare(Ticket ticket){
@@ -10,17 +11,20 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        long inHour = ticket.getInTime().getTime();
-        long outHour = ticket.getOutTime().getTime();
+        long inHour = ticket.getInTime().getTime();  // Data type long + method getTime() is in Milliseconde [ms] 
+        long outHour = ticket.getOutTime().getTime(); // Data type long + method getTime() is in Milliseconde [ms] 
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        long duration = (outHour - inHour)/ 3600000 ;
-        // IN 1 hour = 3600 *1000 milisecond = 3 600 000 Milliseconde
+        double duration = (outHour - inHour)/(1000.0*60*60); // Converting the duration time in hour
+        // method getTime() is in Milliseconde [ms] 
+        //IN 1 hour = 3600 *1000 millisecond = 3 600 000 Milliseconde
         //1 Heure [h] = 3 600 000 Milliseconde [ms] 
-        // Duration in hour = Duration in milisecond   divide by  3 600 000 Milliseconde [ms] 
+        // Duration in hour = Duration in millisecond   divide by  3 600 000 Milliseconde [ms] 
         // DURATION inHOUR =  long inHour / 3 600 000
         // DURATION out HOUR =  long outHour / 3 600 000
         // => long duration IN HOUR = (outHour - inHour) / 3 600 000
+        // (1000.0*60*60)= 3600000 milliseconds = 1 hour
+
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
@@ -30,7 +34,7 @@ public class FareCalculatorService {
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default: throw new IllegalArgumentException("Unknown Parking Type");
         }
     }
 }
